d54a2163ccc64a99aff4c8c0793ca4f7ff9eca61
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-05-27 15:21:52 +0200

    add calcolo finale del prezzo del biglietto

--- js/app.js
+++ js/app.js
@@ -12,3 +12,24 @@ if(isNaN(kmUser)){
 //Recuperiamo l'informazione degli anni dell'utente
 var inputYears = parseInt(prompt('Inserisci i tuoi anni per vedere se puoi usufruire dello sconto'));
 
+var price = 0.25; //prezzo al km
+var subPrice = kmUser * price; //Prezzo totale del biglietto in base i km
+
+var message;
+var discount = 0;
+
+//Controllo sullo sconto del biglietto
+if(inputYears < 18){
+    discount = 20;
+    message = `+ ricevi lo sconto del ${discount}% perchè sei minorenne`;
+} else if(inputYears >= 65){
+    discount = 40;
+    message = `+ ricevi lo sconto del ${discount}% perchè sei over 65`;
+}
+
+discount *= subPrice / 100;
+
+var totalPrice = subPrice - discount;
+
+document.getElementById('ticketPrice')
+.innerHTML += ` ${totalPrice.toFixed(2)} euro ${discount ? message : ''}`;
