3059c8a6eef70a70e117d69759ed4ebe618b5534
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-05-27 14:17:41 +0200

    add logica per recuperare le informazioni dell'utente

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-var kmUser = prompt('Inserisci quanti Km vuoi percorrere');
+//Recuperiamo le informazioni dei km
+var inputKm = prompt('Inserisci solo il numero di Km che vuoi percorrere');
 
-console.log(kmUser);
+//modifichiamo se l'utente ha inserito , al posto del . 
+kmUser = + inputKm.replace(',','.');
+
+if(isNaN(kmUser)){
+    alert('Hai inserito un valore non valido, assicurati di mettere solo il numero dei km, riprova');
+    window.location.reload();
+}
+
+//Recuperiamo l'informazione degli anni dell'utente
+var inputYears = parseInt(prompt('Inserisci i tuoi anni per vedere se puoi usufruire dello sconto'));
+
